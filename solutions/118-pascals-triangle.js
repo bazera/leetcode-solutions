662916d2034d29370b6f5201eeb16be3d62c666d
bazera <bazera.g@gmail.com>
2022-04-13 16:14:52 +0400

    chore: add 118th's solution

---
+++ solutions/118-pascals-triangle.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const arr = [];
+
+  for (let i = 0; i < numRows; i++) {
+    const inner = [];
+
+    for (let j = 0; j < i + 1; j++) {
+      if (i > 0) {
+        const right = arr[i - 1][j];
+        const left = arr[i - 1][j - 1];
+        inner.push((right ?? 0) + (left ?? 0));
+        continue;
+      }
+
+      inner.push(i + 1);
+    }
+
+    arr.push(inner);
+  }
+
+  return arr;
+};
