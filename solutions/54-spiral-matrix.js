5eb90215ce4f930c636dbc17ba0707c2836736d4
bazera <bazera.g@gmail.com>
2022-04-13 13:58:44 +0400

    chore: add 54th's solution

---
+++ solutions/54-spiral-matrix.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+  let m = matrix.length;
+  let n = matrix[0].length;
+
+  let i = (j = start_i = start_j = 0);
+
+  const arr = [];
+
+  counter = 0;
+
+  while (arr.length < matrix.length * matrix[0].length) {
+    arr.push(matrix[i][j]);
+
+    if (i === counter + 0 && j !== n - 1 - counter) {
+      j++;
+      continue;
+    }
+
+    if (j === n - 1 - counter && i !== m - 1 - counter) {
+      i++;
+      continue;
+    }
+
+    if (i === m - 1 - counter && j !== 0 + counter) {
+      j--;
+      continue;
+    }
+
+    if (j === 0 + counter && i !== 1 + counter) {
+      i--;
+      continue;
+    }
+
+    if (i === counter + 1) {
+      j++;
+      counter++;
+      continue;
+    }
+  }
+
+  return arr;
+};
