cb7050dbb46a2920cbf3bdae738208d7f662050c
bazera <bazera.g@gmail.com>
2022-07-09 12:10:45 +0400

    chore: add 206th

---
+++ solutions/206-reverse-linked-list.js
@@ -0,0 +1,18 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
